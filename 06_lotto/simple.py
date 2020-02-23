09ae5bdbf4337aa90699784a0ac3c82b3a6f9867
jinyoung <sjy049@gmail.com>
2020-02-23 15:52:49 +0900

    simple lotto number generation

---
+++ 06_lotto/simple.py
@@ -0,0 +1,15 @@
+import random
+
+lotto = []
+
+rand_num = random.randint(1, 46)
+
+for i in range(6):
+    # 중복방지
+    while rand_num in lotto:
+        rand_num = random.randint(1, 46)
+    lotto.append(rand_num)
+
+lotto.sort()
+
+print('로또번호 : {}'.format(lotto))
